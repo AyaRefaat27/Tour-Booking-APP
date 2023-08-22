fe0d859f849b09e77f53e418582bd4c662e403f9
Aya Refaat <114300685+AyaRefaat27@users.noreply.github.com>
2023-08-22 20:26:48 +0300

    fetch_api

--- Frontend/src/Component/FeaturesTours/toursList.jsx
+++ Frontend/src/Component/FeaturesTours/toursList.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import TourCard from "../../Shared/tourCard";
-import Tours from "../../assets/data/tours";
-
 import { Col } from "react-bootstrap";
+import TourCard from "../../Shared/tourCard";
+import FetchData from "../../Hooks/fetchData.js";
+import { BASE_URL } from "../../utils/config.js";
 
 export default function ToursList() {
+  const {
+    data: featuredTours,
+    loading,
+    error,
+  } = FetchData(`${BASE_URL}/tours/search/getFeaturedTours`);
+
+  // console.log(featuredTours);
   return (
     <>
-      {Tours?.map((tour) => {
-        return (
-          <Col lg="3" className="mb-4" key={tour.id}>
-            <TourCard tour={tour} />
-          </Col>
-        );
-      })}
+      {loading && <h4> Loading ..... </h4>}
+
+      {error && <h4> {error} </h4>}
+      {!loading &&
+        !error &&
+        featuredTours?.map((tour) => {
+          return (
+            <Col lg="3" className="mb-4" key={tour._id}>
+              <TourCard tour={tour} />
+            </Col>
+          );
+        })}
     </>
   );
 }
